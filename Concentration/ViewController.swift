c61270688bb9e2758143da3b98d612ea9110dec3
Laila Mauhibah <laila.mauhibah@gmail.com>
2018-04-26 14:55:52 +0900

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Mauhibah Laila on 26/04/18.
+//  Copyright © 2018 Laila Mauhibah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
